50fcfe248114bca9e2ef96633756b07ac239b57d
Edu <eduardogarciagrande@gmail.com>
2018-02-13 14:31:30 +0100

    Addresses management + UI + refactoring

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 
-from accounts.models import GuestEmail
 from products.models import Product
+from addresses.models import Address
+from addresses.forms import AddressForm
 from orders.models import Order
 from accounts.forms import LoginForm, GuestForm
 from billing.models import BillingProfile
@@ -39,38 +40,32 @@ def checkout_home(request):
     if cart_created or cart_obj.products.count() == 0:
         return redirect("cart:home")
 
-    user = request.user
-    billing_profile = None
     login_form = LoginForm()
     guest_form = GuestForm()
-    guest_email_id = request.session.get('guest_email_id')
+    address_form = AddressForm()
 
-    if user.is_authenticated:
-        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
-    elif guest_email_id is not None:
-        guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
-        billing_profile, billing_guest_profile_created =\
-            BillingProfile.objects.get_or_create(email=guest_email_obj.email)
-    else:
-        pass
+    billing_address_id = request.session.get("billing_address_id", None)
+    shipping_address_id = request.session.get("shipping_address_id", None)
+
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
 
     if billing_profile is not None:
-        order_qs = Order.objects.filter(billing_profile=billing_profile, cart=cart_obj, active=True)
-
-        if order_qs.count() == 1:
-            order_obj = order_qs.first()
-        else:
-            old_order_qs = Order.objects.exclude(billing_profile=billing_profile).filter( cart=cart_obj, active=True)
-            if old_order_qs.exists():
-                old_order_qs.update(active=False)
-
-            order_obj = Order.objects.create(billing_profile=billing_profile, cart=cart_obj)
+        order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
+        if shipping_address_id:
+            order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
+            del request.session["shipping_address_id"]
+        if billing_address_id:
+            order_obj.billing_address = Address.objects.get(id=billing_address_id)
+            del request.session["billing_address_id"]
+        if shipping_address_id or billing_address_id:
+            order_obj.save()
 
     context = {
         'object': order_obj,
         'billing_profile': billing_profile,
         'login_form': login_form,
-        'guest_form': guest_form
+        'guest_form': guest_form,
+        'address_form': address_form,
     }
 
     return render(request, 'carts/checkout.html', context)
--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #Custom apps
     'accounts',
+    'addresses',
     'billing',
     'products',
     'search',
