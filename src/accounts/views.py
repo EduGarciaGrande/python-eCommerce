172f4ed1133734767fd895191aa9e47ae604f015
Edu <eduardogarciagrande@gmail.com>
2018-02-24 16:56:38 +0100

    eCommerce Analytics (part 2)

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -5,6 +5,7 @@ from django.views.generic import CreateView, FormView
 
 from .models import GuestEmail
 from .forms import LoginForm, RegisterForm, GuestForm
+from .signals import user_logged_in
 
 
 def guest_register_view(request):
@@ -49,6 +50,7 @@ class LoginView(FormView):
 
         if user is not None:
             login(request, user)
+            user_logged_in.send(user.__class__, instance=user, request=request)
 
             try:
                 del request.session['guest_email_id']
