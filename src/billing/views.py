b064252090d35202205daaf78974de5bbea671eb
Edu <eduardogarciagrande@gmail.com>
2018-03-02 13:17:12 +0100

    Charge process implemented

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from django.utils.http import is_safe_url
+
+from billing.models import BillingProfile, Card
+
 import stripe
 
 
@@ -11,6 +14,14 @@ STRIPE_PUB_KEY = 'pk_test_Ieryuhr7WQ5oPxm9hr0ECJpb'
 
 
 def payment_method_view(request):
+    # if request.user.is_authenticated:
+    #     billing_profile = request.user.billingprofile
+    #     my_customer_id = billing_profile.customer_id
+
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect("/cart")
+
     next_ = request.GET.get('next')
     next_url = None
 
@@ -22,7 +33,16 @@ def payment_method_view(request):
 
 def payment_method_createview(request):
     if request.method == 'POST' and request.is_ajax():
-        print(request.POST)
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+
+        if not billing_profile:
+            return HttpResponse({"message": "Cannot find this user"}, status_code=401)
+
+        token = request.POST.get("token")
+
+        if token is not None:
+            new_card_obj = Card.objects.add_new(billing_profile, token)
+
         return JsonResponse({'message': 'Success! Your card was added.'})
 
     return HttpResponse('error', status_code=401)
